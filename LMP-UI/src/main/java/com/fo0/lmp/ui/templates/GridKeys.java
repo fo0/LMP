25e36f73ac31dedf4b9d2bc0e4974e2834a28c7b
fo0 <fo0@fo0.me>
2020-03-12 23:56:45 +0100

    added new keys feature to backup menu for import/export configs

--- LMP-UI/src/main/java/com/fo0/lmp/ui/templates/GridKeys.java
+++ LMP-UI/src/main/java/com/fo0/lmp/ui/templates/GridKeys.java
@@ -3,16 +3,11 @@ package com.fo0.lmp.ui.templates;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.Future;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.vaadin.viritin.grid.MGrid;
 
 import com.fo0.lmp.ui.data.KeyManager;
-import com.fo0.lmp.ui.enums.ELinuxActions;
 import com.fo0.lmp.ui.enums.EWindowSize;
-import com.fo0.lmp.ui.model.Action;
-import com.fo0.lmp.ui.model.Host;
 import com.fo0.lmp.ui.model.Key;
 import com.fo0.lmp.ui.utils.UtilsComponents;
 import com.fo0.lmp.ui.utils.UtilsWindow;
@@ -20,9 +15,7 @@ import com.vaadin.ui.Component;
 import com.vaadin.ui.MenuBar;
 
 public class GridKeys extends MGrid<Key> {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = -949632134596892202L;
 	private Set<Key> list = new HashSet<Key>();
 
@@ -43,29 +36,11 @@ public class GridKeys extends MGrid<Key> {
 			setItems(list);
 		}
 
-		addColumn(e -> "Linux").setId("os").setCaption("Platform");
-
-//		addComponentColumn(e -> {
-//			MHorizontalLayout labels = new MHorizontalLayout();
-//			if (e.getHost() != null) {
-//				labels.add(new MLabel().withValue(ICON.HOST.getHtml()).withContentMode(ContentMode.HTML));
-//			}
-//
-//			if (e.getAction() != null) {
-//				labels.add(new MLabel().withValue(ICON.ACTION.getHtml()).withContentMode(ContentMode.HTML));
-//			}
-//
-//			return labels;
-//		}).setId("info").setCaption("Info");
-
 		addComponentColumn(e -> {
 			return addActionButton(e);
-		}).setId("menu").setCaption("Action");
+		}).setId("menu").setCaption("Action").setWidth(150);
 
-		// setColumns("label", "port", "activecheck", "statuscheck", "daysleft", "info",
-		// "menu");
 		setColumns("label", "menu");
-		// sort("url", SortDirection.ASCENDING);
 	}
 
 	public void setList(Set<Key> list) {
@@ -97,12 +72,6 @@ public class GridKeys extends MGrid<Key> {
 		KeyManager.save(list);
 	}
 
-	private void execute(Host host, Action action) {
-		UtilsWindow.createWindow("Multi-Console",
-				new MultiHostConsole(Stream.of(host).collect(Collectors.toSet()), true, action.getCommand()),
-				EWindowSize.Wide, true);
-	}
-
 	private MenuBar addActionButton(Key key) {
 		return UtilsComponents.multiActionButton(cmd -> {
 
@@ -123,14 +92,7 @@ public class GridKeys extends MGrid<Key> {
 				break;
 			}
 
-		}, "Edit", "Delete");
-	}
-
-	private void execute(Key key, ELinuxActions action) {
-		// TODO: Checke alle Zertifikate
-//		UtilsWindow.createWindow("Multi-Console",
-//				new MultiHostConsole(Stream.of(key).collect(Collectors.toSet()), true, action.getCmd()),
-//				EWindowSize.Wide, true);
+		}, "", "Delete");
 	}
 
 	public Thread runWhileAttached(final Component component, final Runnable task, final long interval,
